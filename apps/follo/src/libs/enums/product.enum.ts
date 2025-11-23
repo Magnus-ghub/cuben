b886a6468e0bae6b297a3851316055cc42f73fe7
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-23 14:56:04 +0900

    feat:  create Schema models and project enums

---
+++ apps/follo/src/libs/enums/product.enum.ts
@@ -0,0 +1,34 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum ProductType {
+	APARTMENT = 'APARTMENT',
+	VILLA = 'VILLA',
+	HOUSE = 'HOUSE',
+}
+registerEnumType(ProductType, {
+	name: 'ProductType',
+});
+
+export enum ProductStatus {
+	ACTIVE = 'ACTIVE',
+	SOLD = 'SOLD',
+	DELETE = 'DELETE',
+}
+registerEnumType(ProductStatus, {
+	name: 'ProductStatus',
+});
+
+export enum ProductLocation {
+	SEOUL = 'SEOUL',
+	BUSAN = 'BUSAN',
+	INCHEON = 'INCHEON',
+	DAEGU = 'DAEGU',
+	GYEONGJU = 'GYEONGJU',
+	GWANGJU = 'GWANGJU',
+	CHONJU = 'CHONJU',
+	DAEJON = 'DAEJON',
+	JEJU = 'JEJU',
+}
+registerEnumType(ProductLocation, {
+	name: 'ProductLocation',
+});

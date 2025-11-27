ebd8b1f3a73bcf99b9563c85bf0f8c93067e2b1a
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-27 14:10:31 +0900

    fix: modify getProduct Api

--- apps/cuben/src/components/product/product.service.ts
+++ apps/cuben/src/components/product/product.service.ts
@@ -14,29 +14,29 @@ import { ProductUpdate } from '../../libs/dto/product/product.update';
 
 @Injectable()
 export class ProductService {
-    constructor(
-        @InjectModel('Product') private readonly productModel: Model<Product>,
-        private memberService: MemberService,
-        private viewService: ViewService,
-        //like service
-    ) {}
+	constructor(
+		@InjectModel('Product') private readonly productModel: Model<Product>,
+		private memberService: MemberService,
+		private viewService: ViewService,
+		//like service
+	) {}
 
-    public async createProduct(input: ProductInput): Promise<Product> {
-        try {
-            const result = await this.productModel.create(input);
-            await this.memberService.memberStatsEditor({
-                _id: result.memberId,
-                targetKey: 'memberProducts',
-                modifier: 1,
-            });
-            return result;
-        } catch (err) {
-            console.log('Error, Service.model:', err.message);
-            throw new BadRequestException(Message.CREATE_FAILED);
-        }
-    }
+	public async createProduct(input: ProductInput): Promise<Product> {
+		try {
+			const result = await this.productModel.create(input);
+			await this.memberService.memberStatsEditor({
+				_id: result.memberId,
+				targetKey: 'memberProducts',
+				modifier: 1,
+			});
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 
-    public async getProduct(memberId: ObjectId, productId: ObjectId): Promise<Product> {
+	public async getProduct(memberId: ObjectId, productId: ObjectId): Promise<Product> {
         const search: T = {
             _id: productId,
             productStatus: ProductStatus.ACTIVE,
@@ -44,50 +44,58 @@ export class ProductService {
 
         const targetProduct: Product = await this.productModel.findOne(search).lean().exec();
         if(!targetProduct) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        console.log("'targetProduct:", targetProduct);
 
-        targetProduct.memberData = await this.memberService.getMember(null, targetProduct.memberId);
-        return targetProduct;
-    }
-    
-    public async updateProduct(memberId: ObjectId, input: ProductUpdate): Promise<Product> {
-        let { productStatus, soldAt, deletedAt } = input;
-        const search: T =  {
-            _id: input._id,
-            memberId: memberId,
-            propertyStatus: ProductStatus.ACTIVE,
-        };
-
-        if (productStatus === ProductStatus.SOLD) soldAt = moment().toDate();
-        else if (productStatus === ProductStatus.DELETE) deletedAt = moment().toDate();
-
-        const result = await this.productModel
-            .findOneAndUpdate(search, input, { new: true, })
-            .exec();
-        if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
-        
-        if(soldAt || deletedAt) {
-            await this.memberService.memberStatsEditor({
-                _id: memberId,
-                targetKey: 'memberProducts',
-                modifier: -1,
-            });
+        if(memberId) {
+            const viewInput = { memberId: memberId, viewRefId: productId, viewGroup: ViewGroup.PRODUCT};
+            const newView = await this.viewService.recordView(viewInput);
+            if(newView) {
+                await this.productStatsEditor({ _id: productId, targetKey: 'productViews', modifier: 1});
+                targetProduct.productViews++;
+            }
+            
+            //TODO: meLiked
         }
 
-     return result;
+        targetProduct.memberData = await this.memberService.getMember(null, targetProduct.memberId).catch(() => null);
+        return targetProduct;
     }
 
+	public async updateProduct(memberId: ObjectId, input: ProductUpdate): Promise<Product> {
+		let { productStatus, soldAt, deletedAt } = input;
+		const search: T = {
+			_id: input._id,
+			memberId: memberId,
+			productStatus: ProductStatus.ACTIVE,
+		};
 
-    public async productStatsEditor(input: StatisticModifier): Promise<Product> {
-        const { _id, targetKey, modifier } = input;
-        return await this.productModel
-            .findByIdAndUpdate(
-                _id,
-                { $inc: { [targetKey]: modifier } },
-                {
-                    new: true,
-                },
-            )
-            .exec();
-    }
+		if (productStatus === ProductStatus.SOLD) soldAt = moment().toDate();
+		else if (productStatus === ProductStatus.DELETE) deletedAt = moment().toDate();
 
+		const result = await this.productModel.findOneAndUpdate(search, input, { new: true }).exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		if (soldAt || deletedAt) {
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberProducts',
+				modifier: -1,
+			});
+		}
+
+		return result;
+	}
+
+	public async productStatsEditor(input: StatisticModifier): Promise<Product> {
+		const { _id, targetKey, modifier } = input;
+		return await this.productModel
+			.findByIdAndUpdate(
+				_id,
+				{ $inc: { [targetKey]: modifier } },
+				{
+					new: true,
+				},
+			)
+			.exec();
+	}
 }

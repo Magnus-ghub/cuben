df5cf6e3a96c25abf9a637ac07fadd19e1a64937
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-27 13:07:11 +0900

    feat: develop getProduct and updateProduct GraphQL Apis

--- apps/cuben/src/components/product/product.resolver.ts
+++ apps/cuben/src/components/product/product.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProductService } from './product.service';
 import { Product } from '../../libs/dto/product/product';
 import { ProductInput } from '../../libs/dto/product/product.input';
@@ -8,20 +8,46 @@ import { UseGuards } from '@nestjs/common';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { ProductUpdate } from '../../libs/dto/product/product.update';
 
 @Resolver()
 export class ProductResolver {
-    constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) {}
 
-    @Roles(MemberType.USER, MemberType.ADMIN)
+	@Roles(MemberType.USER, MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Product)
+	public async createProduct(
+		@Args('input') input: ProductInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Product> {
+		console.log('Mutation: createProduct');
+		input.memberId = memberId;
+		return await this.productService.createProduct(input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Product)
+	public async getProduct(
+        @Args('productId') input: string, 
+        @AuthMember('_id') memberId: ObjectId
+    ): Promise<Product> {
+		console.log('Query: getProduct');
+		const productId = shapeIntoMongoObjectId(input);
+		return await this.productService.getProduct(memberId, productId);
+	}
+
+    @Roles(MemberType.USER) 
     @UseGuards(RolesGuard)
-    @Mutation(() => Product)
-    public async createProduct(
-        @Args('input') input: ProductInput,
+    @Mutation((returns) => Product)
+    public async updateProduct(
+        @Args('input') input: ProductUpdate,
         @AuthMember('_id') memberId: ObjectId,
     ): Promise<Product> {
-        console.log('Mutation: createProduct');
-        input.memberId = memberId;
-        return await this.productService.createProduct(input);
+        console.log('Query: updateProduct');
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.productService.updateProduct(memberId, input);
     }
 }
--- apps/cuben/src/components/product/product.service.ts
+++ apps/cuben/src/components/product/product.service.ts
@@ -1,11 +1,16 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Product } from '../../libs/dto/product/product';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
 import { ProductInput } from '../../libs/dto/product/product.input';
 import { Message } from '../../libs/enums/common.enum';
+import moment from 'moment';
+import { StatisticModifier, T } from '../../libs/types/common';
+import { ProductStatus } from '../../libs/enums/product.enum';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { ProductUpdate } from '../../libs/dto/product/product.update';
 
 @Injectable()
 export class ProductService {
@@ -30,4 +35,59 @@ export class ProductService {
             throw new BadRequestException(Message.CREATE_FAILED);
         }
     }
+
+    public async getProduct(memberId: ObjectId, productId: ObjectId): Promise<Product> {
+        const search: T = {
+            _id: productId,
+            productStatus: ProductStatus.ACTIVE,
+        };
+
+        const targetProduct: Product = await this.productModel.findOne(search).lean().exec();
+        if(!targetProduct) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        targetProduct.memberData = await this.memberService.getMember(null, targetProduct.memberId);
+        return targetProduct;
+    }
+    
+    public async updateProduct(memberId: ObjectId, input: ProductUpdate): Promise<Product> {
+        let { productStatus, soldAt, deletedAt } = input;
+        const search: T =  {
+            _id: input._id,
+            memberId: memberId,
+            propertyStatus: ProductStatus.ACTIVE,
+        };
+
+        if (productStatus === ProductStatus.SOLD) soldAt = moment().toDate();
+        else if (productStatus === ProductStatus.DELETE) deletedAt = moment().toDate();
+
+        const result = await this.productModel
+            .findOneAndUpdate(search, input, { new: true, })
+            .exec();
+        if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+        
+        if(soldAt || deletedAt) {
+            await this.memberService.memberStatsEditor({
+                _id: memberId,
+                targetKey: 'memberProducts',
+                modifier: -1,
+            });
+        }
+
+     return result;
+    }
+
+
+    public async productStatsEditor(input: StatisticModifier): Promise<Product> {
+        const { _id, targetKey, modifier } = input;
+        return await this.productModel
+            .findByIdAndUpdate(
+                _id,
+                { $inc: { [targetKey]: modifier } },
+                {
+                    new: true,
+                },
+            )
+            .exec();
+    }
+
 }

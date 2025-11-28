7cc6290c4b238075575ed091708e0b582a872dcd
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-29 00:03:18 +0900

    feat: create getMemberFollowings GraphQL Api

--- apps/cuben/src/components/follow/follow.resolver.ts
+++ apps/cuben/src/components/follow/follow.resolver.ts
@@ -1,35 +1,43 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { FollowService } from './follow.service';
-import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
-import { Follower } from '../../libs/dto/follow/follow';
+import { Follower, Followings } from '../../libs/dto/follow/follow';
 import { ObjectId } from 'mongoose';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { FollowInquiry } from '../../libs/dto/follow/follow.input';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
 export class FollowResolver {
-    constructor(private readonly followService: FollowService) {}
+	constructor(private readonly followService: FollowService) {}
 
-    @UseGuards(AuthGuard)
-    @Mutation((returns) => Follower)
-    public async subscribe(
-        @Args('input') input: string,
-        @AuthMember('_id') memberId: ObjectId,
-    ): Promise<Follower> {
-        console.log('Mutation: Subscribe');
-        const followingId = shapeIntoMongoObjectId(input);
-        return await this.followService.subscribe(memberId, followingId);
-    }
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Follower)
+	public async subscribe(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Follower> {
+		console.log('Mutation: Subscribe');
+		const followingId = shapeIntoMongoObjectId(input);
+		return await this.followService.subscribe(memberId, followingId);
+	}
 
-    @UseGuards(AuthGuard)
-    @Mutation((returns) => Follower)
-    public async unsubscribe(
-        @Args('input') input: string,
-        @AuthMember('_id') memberId: ObjectId,
-    ): Promise<Follower> {
-        console.log('Mutation: unsubscribe');
-        const followingId = shapeIntoMongoObjectId(input);
-        return await this.followService.unsubscribe(memberId, followingId);
-    }
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Follower)
+	public async unsubscribe(@Args('input') input: string, @AuthMember('_id') memberId: ObjectId): Promise<Follower> {
+		console.log('Mutation: unsubscribe');
+		const followingId = shapeIntoMongoObjectId(input);
+		return await this.followService.unsubscribe(memberId, followingId);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query((returns) => Followings)
+	public async getMemberFollowings(
+		@Args('input') input: FollowInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Followings> {
+		console.log('Query: getMemberFollowings');
+		const { followerId } = input.search;
+        input.search.followerId = shapeIntoMongoObjectId(followerId);
+		return await this.followService.getMemberFollowings(memberId, input);
+	}
 }

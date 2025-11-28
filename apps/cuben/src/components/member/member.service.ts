f5c468197756be78ab58c4eb2b33958cd63d5336
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-29 02:06:51 +0900

    feat: fix: modify meFollowed logic

--- apps/cuben/src/components/member/member.service.ts
+++ apps/cuben/src/components/member/member.service.ts
@@ -14,11 +14,13 @@ import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { lookupAuthMemberLiked } from '../../libs/config';
+import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService {
 	constructor(
 		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		@InjectModel('Follow') private readonly followModel: Model<Follower | Following>, 
 		private authService: AuthService,
 		private viewService: ViewService,
 		private likeService: LikeService,
@@ -74,11 +76,16 @@ export class MemberService {
 			const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
             targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
 
-			// TODO: meFollowed
+			targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
 		}
 
 		return targetMember;
 	}
+
+	private async checkSubscription(followerId: ObjectId, followingId: ObjectId): Promise<MeFollowed[]> {
+        const result = await this.followModel.findOne({ followingId: followingId, followerId: followerId }).exec();
+        return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true }] : [];
+    }
 
 	public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member> {
 		const result: Member = await this.memberModel

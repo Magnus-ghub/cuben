d1b94a1b1a538fe1fd572a38908801e275fb4e95
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-29 11:38:53 +0900

    feat: create Comment service model

--- apps/cuben/src/components/comment/comment.service.ts
+++ apps/cuben/src/components/comment/comment.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { MemberService } from '../member/member.service';
+import { ProductService } from '../product/product.service';
+import { BoardArticleService } from '../board-article/board-article.service';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+    constructor(
+        @InjectModel('Comment') private readonly commentModel: Model<null>,
+        private readonly memberServise: MemberService,
+        private readonly productServise: ProductService,
+        private readonly boardArticleServise: BoardArticleService,
+    ){}
+}

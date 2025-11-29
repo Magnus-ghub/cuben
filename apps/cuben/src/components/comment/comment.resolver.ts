0e018bdc1699a072890161a1f2ed9fbc00e761b7
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-11-29 14:15:02 +0900

    feat: create updateComment GraphQL Api

--- apps/cuben/src/components/comment/comment.resolver.ts
+++ apps/cuben/src/components/comment/comment.resolver.ts
@@ -1,7 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { Comment } from '../../libs/dto/comment/comment';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 
 @Resolver()
 export class CommentResolver {
-    constructor(private readonly commentServise: CommentService) {}
+    constructor(private readonly commentService: CommentService) {}
+
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => Comment)
+    public async createComment(
+        @Args('input') input: CommentInput,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Comment> {
+        console.log('Mutation: createComment');
+        return await this.commentService.createComment(input, memberId)
+    }
+
+    @UseGuards(AuthGuard)
+    @Mutation((returns) => Comment)
+    public async updateComment(
+        @Args('input') input: CommentUpdate,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Comment> {
+        console.log('Mutation: updateComment');
+        return await this.commentService.updateComment(memberId, input);
+    }
 }
--- apps/cuben/src/components/comment/comment.service.ts
+++ apps/cuben/src/components/comment/comment.service.ts
@@ -1,16 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { MemberService } from '../member/member.service';
 import { ProductService } from '../product/product.service';
 import { BoardArticleService } from '../board-article/board-article.service';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { Message } from '../../libs/enums/common.enum';
+import { Comment } from '../../libs/dto/comment/comment';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 
 @Injectable()
 export class CommentService {
     constructor(
-        @InjectModel('Comment') private readonly commentModel: Model<null>,
-        private readonly memberServise: MemberService,
-        private readonly productServise: ProductService,
-        private readonly boardArticleServise: BoardArticleService,
+        @InjectModel('Comment') private readonly commentModel: Model<Comment>,
+        private readonly memberService: MemberService,
+        private readonly productService: ProductService,
+        private readonly boardArticleService: BoardArticleService,
     ){}
+
+    public async createComment(input: CommentInput, memberId: ObjectId): Promise<Comment> {
+        input.memberId = memberId;
+
+        let result = null;
+        try {
+            result = await this.commentModel.create(input);
+        } catch (err) {
+            console.log('Error, Service.model:', err.message);
+            throw new BadRequestException(Message.CREATE_FAILED);
+        }
+
+        switch (input.commentGroup) {
+            case CommentGroup.PRODUCT:
+                await this.productService.productStatsEditor({
+                    _id: input.commentRefId,
+                    targetKey: 'productComments',
+                    modifier: 1,
+                });
+                break;
+            case CommentGroup.ARTICLE:
+                await this.boardArticleService.boardArticleStatsEditor({
+                    _id: input.commentRefId,
+                    targetKey: 'articleComments',
+                    modifier: 1,
+                })
+                break;
+            case CommentGroup.MEMBER:
+                await this.memberService.memberStatsEditor({
+                    _id: input.commentRefId,
+                    targetKey: 'memberComments',
+                    modifier: 1,
+                });
+                break;
+        }
+
+        if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+        return result;
+    }
+
+    public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
+            let { _id } = input;
+          
+            const result = await this.commentModel.findOneAndUpdate(
+                {
+                    _id: _id,
+                    memberId: memberId,
+                    commentStatus: CommentStatus.ACTIVE
+                },
+                input,
+                {
+                    new: true,
+                },
+            )
+            .exec();
+            if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+            return result;
+        }
 }

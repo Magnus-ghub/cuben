fec6e07c0ae8012ea6395fac4937ac2ff4dad445
ilhom nomozov <nomozof@IlhomMacBook.local>
2026-02-14 13:24:59 +0900

    fix: modify main.ts

--- apps/cuben/src/main.ts
+++ apps/cuben/src/main.ts
@@ -7,19 +7,27 @@ import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.use(cookieParser());
-	app.useGlobalPipes(new ValidationPipe());
-	app.useGlobalInterceptors(new LoggingInterceptor());
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new LoggingInterceptor());
 
-	app.enableCors({
-		origin: ['http://localhost:4000', 'https://cuben.info'],
-		credentials: true,
-	});
+    app.enableCors({
+        origin: [
+            'http://localhost:4000', 
+            'https://cuben.info', 
+            'http://72.61.119.128:4000' 
+        ],
+        credentials: true,
+    });
 
-	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-	app.use('/uploads', express.static('./uploads'));
+    app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+    app.use('/uploads', express.static('./uploads'));
 
-	await app.listen(process.env.PORT_API ?? 5007);
+    // DIQQAT: '0.0.0.0' qo'shish Docker uchun shart!
+    const port = process.env.PORT_API ?? 5007;
+    await app.listen(port, '0.0.0.0'); 
+    
+    console.log(`Server is running on port ${port} and accepting external connections.`);
 }
 bootstrap();
